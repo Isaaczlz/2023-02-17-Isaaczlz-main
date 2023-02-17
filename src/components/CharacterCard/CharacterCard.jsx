ab43ed9eb45309122d6a6534051cac481c75d009
Isaac <isaacgonzalez101000@gmail.com>
2023-02-17 16:33:34 -0300

    first commit

---
+++ src/components/CharacterCard/CharacterCard.jsx
@@ -0,0 +1,15 @@
+import "./CharacterCard.css";
+import React from "react";
+// Importar las actions como Object Modules, sino los test no funcionarán!
+
+//PARA QUE LOS TEST CORRAN, DEBEN HACER ESTE COMPONENTE COMO UN FUNCIONAL COMPONENT.
+
+const CharacterCard = (props) => {
+  return (
+    <div className="card">
+      <button>X</button>
+    </div>
+  );
+};
+
+export default CharacterCard;
